92b3b2eedace49df90e7a105edc3df043605de74
dmora1277 <dmora1277@001LAB102010542.neisd.ad>
2015-03-05 15:35:06 -0600

    added code for submenu

--- Zoo Layout/src/zoolayout.java
+++ Zoo Layout/src/zoolayout.java
@@ -1,4 +1,150 @@
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class zoolayout {
+import javax.swing.JLabel;
+import javax.swing.GroupLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
+
+public class zoolayout extends JFrame {
+
+	JLabel label;
+    public zoolayout() {
+        
+        initUI();
+    }
+
+    private void initUI() {
+    	
+        String lyrics =  "Drawing is Life";
+        
+        label = new JLabel(lyrics);
+        label.setFont(new Font("Serif", Font.PLAIN, 14));
+        label.setForeground(new Color(50, 50, 25));
+
+        createMenuBar();
+
+setTitle("Submenu");
+setSize(360, 250);
+setLocationRelativeTo(null);
+setDefaultCloseOperation(EXIT_ON_CLOSE);
+JButton quitButton = new JButton("Quit");
+
+quitButton.addActionListener(new ActionListener() {
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        System.exit(0);
+    }
+});
+
+createLayout(quitButton);
+createLayout(label);
+
+setTitle("Quit button");
 }
+
+
+private void createLayout(JComponent... arg) {
+
+Container pane = getContentPane();
+GroupLayout gl = new GroupLayout(pane);
+pane.setLayout(gl);        
+
+gl.setAutoCreateContainerGaps(true);
+
+gl.setHorizontalGroup(gl.createSequentialGroup()
+        .addComponent(arg[0])
+);
+
+gl.setVerticalGroup(gl.createParallelGroup()
+        .addComponent(arg[0])
+);
+
+pack();
+} 
+    
+    private void createMenuBar() {
+        
+        JMenuBar menubar = new JMenuBar();
+        
+        ImageIcon iconNew = new ImageIcon("new.png");
+        ImageIcon iconOpen = new ImageIcon("open.png");
+        ImageIcon iconSave = new ImageIcon("save.png");
+        ImageIcon iconExit = new ImageIcon("exit.png");
+
+        JMenu fileMenu = new JMenu("Menu");
+
+        JMenu impMenu = new JMenu("Import");
+
+        JMenuItem newsfMi = new JMenuItem("Import newsfeed list...");
+        JMenuItem bookmMi = new JMenuItem("Import bookmarks...");
+        JMenuItem mailMi = new JMenuItem("Import mail...");
+
+        impMenu.add(newsfMi);
+        impMenu.add(bookmMi);
+        impMenu.add(mailMi);
+
+        JMenuItem newMi = new JMenuItem("New Document", iconNew);
+        JMenuItem openMi = new JMenuItem("Open Document", iconOpen);
+        JMenuItem saveMi = new JMenuItem("Save Document", iconSave);
+
+        JMenuItem exitMi = new JMenuItem("Exit Submenu", iconExit);
+        exitMi.setToolTipText("Exit application");
+
+        exitMi.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                System.exit(0);
+            }
+        });
+
+        fileMenu.add(newMi);
+        fileMenu.add(openMi);
+        fileMenu.add(saveMi);
+        fileMenu.addSeparator();
+        fileMenu.add(impMenu);
+        fileMenu.addSeparator();
+        fileMenu.add(exitMi);
+
+        menubar.add(fileMenu);
+
+        setJMenuBar(menubar);        
+    }
+    
+    public static void main(String[] args) {
+        
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                
+                zoolayout ex = new zoolayout();
+                ex.setVisible(true);
+            }
+        });
+    }
+}
+
+            
+            
+        
+            
+
+   
+            
+            
+   
+            
+        
+    
+
+
